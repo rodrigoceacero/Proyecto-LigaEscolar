bd86ee519e96081bc7f67f34cd2eaca0b05ce756
rodrigo <rceaare426@g.educaand.es>
2024-05-03 14:24:39 +0200

    Entity Person creada

---
+++ src/Entity/Person.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'person')]
+class Person
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+    #[ORM\Column(type: 'string')]
+    private string $firstName;
+    #[ORM\Column(type: 'string')]
+    private string $lastName;
+    #[ORM\Column(type: 'integer')]
+    private int $number;
+    #[ORM\Column(type: 'boolean')]
+    private bool $isPlayer;
+    #[ORM\Column(type: 'boolean')]
+    private bool $isTeacher;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getNumber(): int
+    {
+        return $this->number;
+    }
+
+    public function setNumber(int $number): void
+    {
+        $this->number = $number;
+    }
+
+    public function isPlayer(): bool
+    {
+        return $this->isPlayer;
+    }
+
+    public function setIsPlayer(bool $isPlayer): void
+    {
+        $this->isPlayer = $isPlayer;
+    }
+
+    public function isTeacher(): bool
+    {
+        return $this->isTeacher;
+    }
+
+    public function setIsTeacher(bool $isTeacher): void
+    {
+        $this->isTeacher = $isTeacher;
+    }
+
+}
