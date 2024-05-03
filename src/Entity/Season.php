6fa8c2cfef70b2d1efb5f11bcd16eae55cb52f45
rodrigo <rceaare426@g.educaand.es>
2024-05-03 14:24:50 +0200

    Entity Season creada

---
+++ src/Entity/Season.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'season')]
+class Season
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+    #[ORM\Column(type: 'string')]
+    private string $description;
+    #[ORM\Column(type: 'datetime')]
+    private \DateTime $startDate;
+    #[ORM\Column(type: 'datetime')]
+    private \DateTime $endDate;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getStartDate(): \DateTime
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(\DateTime $startDate): void
+    {
+        $this->startDate = $startDate;
+    }
+
+    public function getEndDate(): \DateTime
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(\DateTime $endDate): void
+    {
+        $this->endDate = $endDate;
+    }
+
+}
+
