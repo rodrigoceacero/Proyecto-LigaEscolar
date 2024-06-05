20c48c83ba4613f21d94d1ce080c0efe18102d79
rodrigoceare <rceaare426@g.educaand.es>
2024-06-05 03:51:47 +0200

    Añadida tarjeta de usuarios

---
+++ src/Controller/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name:'users')]
+    public function index() :Response
+    {
+        return $this->render('users/list.html.twig');
+    }
+}
