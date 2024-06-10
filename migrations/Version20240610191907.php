eb7cc7ba0f021ff8fd8075b90986c42471465680
rodrigoceare <rceaare426@g.educaand.es>
2024-06-10 21:20:35 +0200

    Añadido campo active a Sport y eliminado number en Person

---
+++ migrations/Version20240610191907.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240610191907 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE person DROP number');
+        $this->addSql('ALTER TABLE sport ADD active TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE sport DROP active');
+        $this->addSql('ALTER TABLE person ADD number INT NOT NULL');
+    }
+}
